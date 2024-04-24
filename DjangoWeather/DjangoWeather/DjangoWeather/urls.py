e9d66c0e82e82b58c7f336531463dea6af157d88
Kevin Legrand <kevinlegrand0113@gmail.com>
2024-04-24 20:27:54 +0200

    Init project

--- DjangoWeather/DjangoWeather/DjangoWeather/urls.py
+++ DjangoWeather/DjangoWeather/DjangoWeather/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ParisEval project.
+URL configuration for DjangoWeather project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- DjangoWeather/DjangoWeather/DjangoWeather/wsgi.py
+++ DjangoWeather/DjangoWeather/DjangoWeather/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ParisEval project.
+WSGI config for DjangoWeather project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ParisEval.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoWeather.settings')
 
 application = get_wsgi_application()
