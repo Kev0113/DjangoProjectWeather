af291b99d8f186650530734cc989bea3aba4cb40
Thibaut <thibautdebroucke@gmail.com>
2024-04-26 10:51:08 +0200

    home

--- DjangoWeather/DjangoWeather/base/models.py
+++ DjangoWeather/DjangoWeather/base/models.py
@@ -1,6 +1,1 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-class AuthUser(AbstractUser):
-    # Ajoutez vos champs personnalisés ici
-    money = models.CharField(max_length=100, blank=True)
