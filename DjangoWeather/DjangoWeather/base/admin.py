6430abf4c0d2fe396327ab06d18a68b332f79719
Kevin Legrand <kevinlegrand0113@gmail.com>
2024-04-26 12:02:28 +0200

    style

--- DjangoWeather/DjangoWeather/base/admin.py
+++ DjangoWeather/DjangoWeather/base/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from base.models import *
+from django.contrib.auth.models import AbstractUser, Group, Permission, User
+
 
 # Register your models here.
+@admin.register( UserMoney )
+class MoneyUserAdmin(admin.ModelAdmin):
+
+    list_display = ('Username', 'money')
+
+    def Username(self, obj):
+        utilisateur = User.objects.get(id=obj.user_id)
+        return utilisateur.username
