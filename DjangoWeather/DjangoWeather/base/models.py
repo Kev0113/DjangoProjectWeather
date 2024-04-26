7442b83ada113b4b2860a333723904c3e5c3f262
Kevin Legrand <kevinlegrand0113@gmail.com>
2024-04-26 10:50:57 +0200

    azertyui

--- DjangoWeather/DjangoWeather/base/models.py
+++ DjangoWeather/DjangoWeather/base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission, User
 
 # Create your models here.
+class UserMoney(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    money = models.IntegerField(default=100)
--- DjangoWeather/DjangoWeather/base/views.py
+++ DjangoWeather/DjangoWeather/base/views.py
@@ -3,6 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import UserMoney
+
+
+@receiver(post_save, sender=User)
+def CreateMoneyUser(sender, instance, created, **kwargs):
+    if created:
+        UserMoney.objects.create(user=instance, money=100)
+
 
 @login_required
 def Home(request):
